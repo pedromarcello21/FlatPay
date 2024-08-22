000cbe20dd89c8d426b91f2d53812431344eb89f
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-08-22 12:04:39 -0400

    updated welcome banner

--- client/src/components/UserPanel/UserDetails.js
+++ client/src/components/UserPanel/UserDetails.js
@@ -5,10 +5,12 @@ export default function UserDetails({currentUser, setCurrentUser}) {
 const greetings = ["Hi", "Hey", "Yo", "Sup", "Welcome"]
 const emojis = ["🤝","👋","⭐️" ]
 
+const date = new Date()
 
   return (
     <div>
-        <h2>{greetings[Math.floor(Math.random()*greetings.length)]}, {currentUser.username} {emojis[Math.floor(Math.random()*emojis.length)]}</h2>
+        {date.getDay() == 3 ? <h2>It's humpday, {currentUser.username} 🐪</h2> : <h2>{greetings[Math.floor(Math.random()*greetings.length)]}, {currentUser.username} {emojis[Math.floor(Math.random()*emojis.length)]}</h2>}
+
 
         
     </div>
